89edd44c0ff6b676d2f62f2f3a4934c58660e1a8
mina <minasamy823@gmail.com>
2019-08-31 23:21:35 +0300

    deploying

--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0f4f3ec276762e3f9e9c67d0a743c11d.js"
+  "/precache-manifest.e8ccc9c643fd9e4e8c4835609e3ba9d7.js"
 );
 
 self.addEventListener('message', (event) => {
