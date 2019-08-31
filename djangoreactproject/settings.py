5f82c30b600d90bf78ee7fc40a2e45d0fd261e88
mina <minasamy823@gmail.com>
2019-08-31 21:26:43 +0300

    deploying

--- djangoreactproject/settings.py
+++ djangoreactproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6viixmiljb!l=-u5^(cu2t_r04nsxz840*6%i3&n_q&kegfz^s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','herokudjangodata.herokuapp.com']
 
